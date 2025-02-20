7c85ec9973a2eb7def1a0cda85ab16574bca8b05
Robin Burri <robinburri@tutanota.com>
2025-02-20 17:05:00 +0100

    add application.propreties profile

--- src/main/java/com/tickets_online/tickets/repositories/RegistrationRepository.java
+++ src/main/java/com/tickets_online/tickets/repositories/RegistrationRepository.java
@@ -3,9 +3,11 @@ package com.tickets_online.tickets.repositories;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tickets_online.tickets.models.Registration;
 
+@Repository
 public interface RegistrationRepository extends MongoRepository<Registration, String> {
 
     Optional<Registration> findByTicketCode(String ticketCode);
